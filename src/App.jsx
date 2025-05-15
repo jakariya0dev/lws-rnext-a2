2baa35053fb68ba05d5f06f794ff2e912743f507
jakariya0dev <jhalhadi@gmail.com>
2025-05-15 21:29:46 +0600

    html to jsx done

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+import CreateOrder from "./components/CreateOrder";
+import Navbar from "./components/Navbar";
+import OrderSummery from "./components/OrderSummery";
+
+function App() {
+  return (
+    <div className="container mx-auto px-4 h-screen flex flex-col">
+      <Navbar />
+      <main className="grid grid-cols-1 md:grid-cols-3 gap-6 mt-6 flex-grow">
+        <CreateOrder />
+        <OrderSummery />
+      </main>
+    </div>
+  );
+}
+
+export default App;
