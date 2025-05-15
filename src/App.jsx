7b2f053f23b4ce1e4e7349689da9772249eaed43
jakariya0dev <jhalhadi@gmail.com>
2025-05-15 23:34:23 +0600

    all functionality added without search

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
+import { useState } from "react";
 import "./App.css";
 import CreateOrder from "./components/CreateOrder";
 import Navbar from "./components/Navbar";
 import OrderSummery from "./components/OrderSummery";
 
 function App() {
+  const [orders, setOrders] = useState([]);
+
   return (
     <div className="container mx-auto px-4 h-screen flex flex-col">
       <Navbar />
       <main className="grid grid-cols-1 md:grid-cols-3 gap-6 mt-6 flex-grow">
-        <CreateOrder />
-        <OrderSummery />
+        <CreateOrder setOrders={setOrders} />
+        <OrderSummery orders={orders} setOrders={setOrders} />
       </main>
     </div>
   );
